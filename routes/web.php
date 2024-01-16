9acedb790acad4ac3b8b7ea9c7086ec0e60b2454
abdulrahman jlilati <abdulrahman.jlilati@raizer.tech>
2024-01-16 23:11:54 +0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecordController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('records.index');
 });
+
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::get('/register', 'create')->name('register')->middleware('guest');
+    Route::post('/register', 'store')->name('signup')->middleware('guest');
+
+});
+
+
+Route::controller(SessionController::class)->group(function(){
+
+    Route::get('/login', 'create')->name('login')->middleware('guest');
+    Route::post('/login', 'store')->name('signin')->middleware('guest');
+    Route::get('/logout', 'destroy')->name('logout')->middleware('auth');
+
+
+});
+
+
+Route::controller(RecordController::class)->group(function() {
+    Route::get('/records/index','index')->name('records.index')->middleware('auth');
+    Route::get('/records/create', 'create')->name('records.create')->middleware('auth');
+    Route::post('/records/store', 'store')->name('records.store')->middleware('auth');
+    Route::get('/records/edit/record/{record_id}', 'edit')->name('records.edit')->middleware('auth');
+    Route::post('/records/update', 'update')->name('records.update')->middleware('auth');
+    Route::get('/records/delete/record/{record_id}', 'delete')->name('records.delete')->middleware('auth');
+
+
+});
+
+
