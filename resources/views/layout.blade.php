98ce2e0a8cd65e0a346c126dbae0adcc9ef76b68
unknown <majd.mawas@raizer.tech>
2024-05-20 00:31:07 +0300

    hotfix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -31,7 +31,7 @@
     </script>
     <!--begin::Javascript-->
     <!--begin::Global Javascript Bundle(used by all pages)-->
-    {{-- <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script> --}}
+    <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
     <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
     <!--end::Global Javascript Bundle-->
     <!--begin::Page Vendors Javascript(used by this page)-->
