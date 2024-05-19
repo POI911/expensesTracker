896d9e04ffbc9c9046f961105889bfd70c249472
abdulrahman jlilati <abdulrahman.jlilati@raizer.tech>
2024-05-19 17:43:48 +0300

    remove unused code

---
+++ resources/views/record/index.blade.php
@@ -0,0 +1,335 @@
+@extends('layout')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>My Records</title>
+</head>
+	<!--end::Head-->
+	<!--begin::Body-->
+@section('content')
+
+
+
+@include('navbar')
+		<!--begin::Main-->
+		<!--begin::Root-->
+
+		<div class="container justify-content-center">
+			<!--begin::Page-->
+			<div class="page d-flex flex-row flex-column-fluid">
+
+				<!--begin::Wrapper-->
+				<div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+
+					<!--begin::Content-->
+					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+						<!--begin::Post-->
+						<div class="post d-flex flex-column-fluid" id="kt_post">
+							<!--begin::Container-->
+							<div id="kt_content_container" class="container-xxl">
+								<!--begin::Card-->
+								<div class="card">
+									<!--begin::Card header-->
+									<div class="card-header border-0 pt-6">
+										<!--begin::Card title-->
+										<div class="card-title">
+
+										</div>
+										<!--begin::Card title-->
+										<!--begin::Card toolbar-->
+										<div class="card-toolbar">
+											<!--begin::Toolbar-->
+											<div class="d-flex justify-content-end" data-kt-customer-table-toolbar="base">
+												<!--begin::Filter-->
+												<button type="button" class="btn btn-light-primary me-3" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+												<!--begin::Svg Icon | path: icons/duotune/general/gen031.svg-->
+												<span class="svg-icon svg-icon-2">
+													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+														<path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="black" />
+													</svg>
+												</span>
+												<!--end::Svg Icon-->Filter</button>
+												<!--begin::Menu 1-->
+												<div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px" data-kt-menu="true" id="kt-toolbar-filter">
+													<!--begin::Header-->
+													<div class="px-7 py-5">
+														<div class="fs-4 text-dark fw-bolder">Filter Options</div>
+													</div>
+													<!--end::Header-->
+													<!--begin::Separator-->
+													<div class="separator border-gray-200"></div>
+													<!--end::Separator-->
+													<!--begin::Content-->
+													<div class="px-7 py-5">
+														<!--begin::Input group-->
+														<div class="mb-10">
+															<!--begin::Label-->
+															<label class="form-label fs-5 fw-bold mb-3">Category:</label>
+															<!--end::Label-->
+															<!--begin::Input-->
+															<select class="form-select form-select-solid fw-bolder" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-kt-customer-table-filter="month" data-dropdown-parent="#kt-toolbar-filter">
+																<option></option>
+																<option value="aug">Food & Drinks</option>
+																<option value="sep">Internet & Communication</option>
+																<option value="oct">Transportations</option>
+																<option value="nov">Electricity</option>
+																<option value="dec">Clothes</option>
+															</select>
+															<!--end::Input-->
+														</div>
+														<!--end::Input group-->
+
+														<!--begin::Actions-->
+														<div class="d-flex justify-content-end">
+															<button type="reset" class="btn btn-light btn-active-light-primary me-2" data-kt-menu-dismiss="true" data-kt-customer-table-filter="reset">Reset</button>
+															<button type="submit" class="btn btn-primary" data-kt-menu-dismiss="true" data-kt-customer-table-filter="filter">Apply</button>
+														</div>
+														<!--end::Actions-->
+													</div>
+													<!--end::Content-->
+												</div>
+												<!--end::Menu 1-->
+												<!--end::Filter-->
+
+												<!--begin::Add customer-->
+												<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_customer">New Record</button>
+												<!--end::Add customer-->
+											</div>
+											<!--end::Toolbar-->
+											<!--begin::Group actions-->
+											<div class="d-flex justify-content-end align-items-center d-none" data-kt-customer-table-toolbar="selected">
+												<div class="fw-bolder me-5">
+												<span class="me-2" data-kt-customer-table-select="selected_count"></span>Selected</div>
+												<button type="button" class="btn btn-danger" data-kt-customer-table-select="delete_selected">Delete Selected</button>
+											</div>
+											<!--end::Group actions-->
+										</div>
+										<!--end::Card toolbar-->
+									</div>
+									<!--end::Card header-->
+									<!--begin::Card body-->
+									<div class="card-body pt-0">
+										<!--begin::Table-->
+										<table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_customers_table">
+											<!--begin::Table head-->
+											<thead>
+												<!--begin::Table row-->
+												<tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
+													<th class="w-10px pe-2">
+														<div class="form-check form-check-sm form-check-custom form-check-solid me-3">
+															<input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_customers_table .form-check-input" value="1" />
+														</div>
+													</th>
+													<th class="min-w-125px">Description</th>
+													<th class="min-w-125px">Amount</th>
+													<th class="min-w-125px">Category</th>
+													<th class="min-w-125px">Date</th>
+													<th class="text-end min-w-70px">Actions</th>
+												</tr>
+												<!--end::Table row-->
+											</thead>
+											<!--end::Table head-->
+											<!--begin::Table body-->
+											<tbody class="fw-bold text-gray-600">
+												<tr>
+													<!--begin::Checkbox-->
+													<td>
+														<div class="form-check form-check-sm form-check-custom form-check-solid">
+															<input class="form-check-input" type="checkbox" value="1" />
+														</div>
+													</td>
+													<!--end::Checkbox-->
+													<!--begin::Name=-->
+													<td>
+														<p  class="text-primary  mb-1">White Shirt</p>
+													</td>
+													<!--end::Name=-->
+													<!--begin::Email=-->
+													<td>
+														<p class="text-danger  mb-1">300000</p>
+													</td>
+													<!--end::Email=-->
+													<!--begin::Company=-->
+													<td class="text-success">Clothes</td>
+													<!--end::Company=-->
+
+													<!--begin::Date=-->
+													<td>21 Oct 2023, 5:54 pm</td>
+													<!--end::Date=-->
+													<!--begin::Action=-->
+													<td class="text-end">
+														<a href="#" class="btn btn-sm btn-light btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
+														<!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
+														<span class="svg-icon svg-icon-5 m-0">
+															<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+																<path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="black" />
+															</svg>
+														</span>
+														<!--end::Svg Icon--></a>
+														<!--begin::Menu-->
+														<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4" data-kt-menu="true">
+															<!--begin::Menu item-->
+															<div class="menu-item px-3">
+																<a href="../../demo8/dist/apps/customers/view.html" class="menu-link px-3">Edit</a>
+															</div>
+															<!--end::Menu item-->
+															<!--begin::Menu item-->
+															<div class="menu-item px-3">
+																<a href="#" class="menu-link px-3" data-kt-customer-table-filter="delete_row">Delete</a>
+															</div>
+															<!--end::Menu item-->
+														</div>
+														<!--end::Menu-->
+													</td>
+													<!--end::Action=-->
+												</tr>
+											</tbody>
+											<!--end::Table body-->
+										</table>
+										<!--end::Table-->
+									</div>
+									<!--end::Card body-->
+								</div>
+								<!--end::Card-->
+
+
+								<!--begin::Modals-->
+								<!--begin::Modal - Records - Add-->
+								<div class="modal fade" id="kt_modal_add_customer" tabindex="-1" aria-hidden="true">
+									<!--begin::Modal dialog-->
+									<div class="modal-dialog modal-dialog-centered mw-650px">
+										<!--begin::Modal content-->
+										<div class="modal-content">
+											<!--begin::Form-->
+											<form class="form" action="#" id="kt_modal_add_customer_form" data-kt-redirect="../../demo8/dist/apps/customers/list.html">
+												<!--begin::Modal header-->
+												<div class="modal-header" id="kt_modal_add_customer_header">
+													<!--begin::Modal title-->
+													<h2 class="fw-bolder">Create a record</h2>
+													<!--end::Modal title-->
+													<!--begin::Close-->
+													<div id="kt_modal_add_customer_close" class="btn btn-icon btn-sm btn-active-icon-primary">
+														<!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+														<span class="svg-icon svg-icon-1">
+															<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+																<rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
+																<rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
+															</svg>
+														</span>
+														<!--end::Svg Icon-->
+													</div>
+													<!--end::Close-->
+												</div>
+												<!--end::Modal header-->
+												<!--begin::Modal body-->
+												<div class="modal-body py-10 px-lg-17">
+													<!--begin::Scroll-->
+													<div class="scroll-y me-n7 pe-7" id="kt_modal_add_customer_scroll" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_modal_add_customer_header" data-kt-scroll-wrappers="#kt_modal_add_customer_scroll" data-kt-scroll-offset="300px">
+														<!--begin::Input group-->
+														<div class="fv-row mb-7">
+															<!--begin::Label-->
+															<label class="required fs-6 fw-bold mb-2">Description</label>
+															<!--end::Label-->
+															<!--begin::Input-->
+															<input type="text" class="form-control form-control-solid" placeholder="" name="description" />
+															<!--end::Input-->
+														</div>
+														<!--end::Input group-->
+														<!--begin::Input group-->
+														<div class="fv-row mb-7">
+															<!--begin::Label-->
+															<label class="fs-6 fw-bold mb-2">
+																<span class="required">Amount</span>
+
+															</label>
+															<!--end::Label-->
+															<!--begin::Input-->
+															<input type="number" class="form-control form-control-solid" placeholder="" name="amount" />
+															<!--end::Input-->
+														</div>
+														<!--end::Input group-->
+
+
+														<!--begin::Billing form-->
+														<div id="kt_modal_add_customer_billing_info" class="collapse show">
+
+
+
+															<!--begin::Input group-->
+															<div class="d-flex flex-column mb-7 fv-row">
+																<!--begin::Label-->
+																<label class="fs-6 fw-bold mb-2">
+																	<span class="required">Category</span>
+
+																</label>
+																<!--end::Label-->
+																<!--begin::Input-->
+																<select name="category" aria-label="Select a Category" data-control="select2" data-placeholder="Select a Category..." data-dropdown-parent="#kt_modal_add_customer" class="form-select form-select-solid fw-bolder" required>
+																	<option value="">Select a Category...</option>
+
+																	<option value="ZM">Education</option>
+																	<option value="ZW">Food & Drinks</option>
+                                                                    <option value="ZW">Transportation</option>
+                                                                    <option value="ZW">Electricity</option>
+                                                                    <option value="ZW">Personal Care</option>
+                                                                    <option value="ZW">Home Maintainence</option>
+																</select>
+																<!--end::Input-->
+															</div>
+															<!--end::Input group-->
+
+
+														</div>
+														<!--end::Billing form-->
+													</div>
+													<!--end::Scroll-->
+												</div>
+												<!--end::Modal body-->
+												<!--begin::Modal footer-->
+												<div class="modal-footer flex-center">
+													<!--begin::Button-->
+													<button type="reset" id="kt_modal_add_customer_cancel" class="btn btn-light me-3">Discard</button>
+													<!--end::Button-->
+													<!--begin::Button-->
+													<button type="submit" id="kt_modal_add_customer_submit" class="btn btn-primary">
+														<span class="indicator-label">Submit</span>
+														<span class="indicator-progress">Please wait...
+														<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+													</button>
+													<!--end::Button-->
+												</div>
+												<!--end::Modal footer-->
+											</form>
+											<!--end::Form-->
+										</div>
+									</div>
+								</div>
+								<!--end::Modal - Customers - Add-->
+								<!--end::Modals-->
+							</div>
+							<!--end::Container-->
+						</div>
+						<!--end::Post-->
+					</div>
+					<!--end::Content-->
+
+				</div>
+				<!--end::Wrapper-->
+			</div>
+			<!--end::Page-->
+		</div>
+
+		<!--end::Root-->
+		<!--begin::Drawers-->
+		<!--end::Drawers-->
+		<!--end::Main-->
+
+@endsection
+
+
+
